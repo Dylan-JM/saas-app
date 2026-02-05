36174dc8d82a78be450d669bf44e012d42145e6f
Dylan Marley <dylanjmarley01@gmail.com>
2026-02-05 22:26:06 +0000

    feat: companions list

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,4 @@
 ﻿import React from 'react'
-import {Button} from "@/components/ui/button";
 
 const Page = () => {
     return (
---
+++ components/CTA.tsx
@@ -0,0 +1,9 @@
+﻿const CTA = () => {
+    return (
+        <div>
+            <h1>CTA</h1>
+        </div>
+    )
+}
+
+export default CTA
